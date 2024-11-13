603d397a54c3e4b095f8d5d271ea2b975e68c86e
wxy <470256292@qq.com>
2024-11-13 23:09:41 +0900

    デモサイト用意

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,20 @@
+<script setup>
+import TopNavbar from '../../components/TopNavbar.vue';
+import NewArrivals from '../../components/new-arrivals/NewArrivals.vue';
+</script>
+
+<template src="./home.html"></template>
+<style scoped src="./home.css"></style>
+
+<script>
+export default {
+  components: {
+    TopNavbar,
+  },
+
+  data() {
+    return {
+    };
+  }
+};
+</script>
---
+++ src/pages/user/sign-in/SignIn.vue
@@ -0,0 +1,21 @@
+<script setup>
+import TopNavbar from '../../../components/TopNavbar.vue';
+
+</script>
+
+<template src="./sign-in.html"></template>
+<style scoped src="./sign-in.css"></style>
+
+<script>
+export default {
+  components: {
+    TopNavbar
+  },
+
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
