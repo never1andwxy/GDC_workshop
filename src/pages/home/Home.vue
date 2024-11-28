b5c66ab312b1638cf7e892a4a816d6240a774371
Wang <wang_xinyin@fujitsu.com>
2024-11-28 09:12:56 +0800

    作成途中

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,6 +1,7 @@
 <script setup>
 import TopNavbar from '../../components/TopNavbar.vue';
 import NewArrivals from '../../components/new-arrivals/NewArrivals.vue';
+
 </script>
 
 <template src="./home.html"></template>
@@ -8,12 +9,18 @@ import NewArrivals from '../../components/new-arrivals/NewArrivals.vue';
 
 <script>
 export default {
+  methods: {
+    messageClose(){
+      showMessage: false
+    }
+  },
   components: {
     TopNavbar,
   },
 
   data() {
     return {
+      showMessage: true
     };
   }
 };
